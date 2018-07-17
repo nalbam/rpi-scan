98564f8b728616e0e098142aa62f04758912a381
nalbam <me@nalbam.com>
2018-07-17 15:52:56 +0900

    server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const os = require('os'),
     moment = require('moment-timezone'),
     express = require('express');
 
+const exec = require('child_process').exec;
+const CronJob = require('cron').CronJob;
+
+const token = process.env.LOGZIO_TOKEN || 'EMPTY_TOKEN';
+const type = process.env.LOGZIO_TYPE || 'demo';
+
+const logger = require('logzio-nodejs').createLogger({token: `${token}`, type: `${type}`});
+
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/favicon.ico', express.static('views/favicon.ico'));
@@ -9,9 +17,38 @@ app.use('/favicon.ico', express.static('views/favicon.ico'));
 app.get('/', function (req, res) {
     let host = os.hostname();
     let date = moment().tz('Asia/Seoul').format();
-    res.render('index.ejs', {host: host, date: date, ip: req.ip});
+    res.render('index.ejs', {host: host, date: date, ip: req.ip.split(':').pop()});
 });
 
 app.listen(3000, function () {
     console.log('Listening on port 3000!');
 });
+
+const job = new CronJob({
+    cronTime: '0 * * * * *',
+    onTick: function() {
+        console.log('scan start.');
+        const scan = exec('sudo arp-scan -l | grep -E "([0-9]{1,3}\\.){3}[0-9]{1,3}"');
+        scan.stdout.on('data', data => {
+            // console.log(`${data}`);
+            data.split('\n').forEach(function (item) {
+                const arr = item.split('\t');
+                if (arr && arr[0]) {
+                    logger.log({ip: arr[0], mac: arr[1], desc: arr[2]});
+                }
+                // console.log(`${item}`);
+            });
+            // console.log('scan done.');
+        });
+
+        scan.stderr.on('data', data => {
+            console.log(`Error: ${data}`);
+        });
+    },
+    start: false,
+    timeZone: 'Asia/Seoul'
+});
+
+if (token !== 'EMPTY_TOKEN') {
+    job.start();
+}
